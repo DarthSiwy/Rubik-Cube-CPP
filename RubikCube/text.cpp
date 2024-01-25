a68bcabaff936cdb6150ea2518ad004ec2c65039
Piotr <piotrsiwiec.lfc@outlook.com>
2024-01-25 15:19:06 +0100

    basic changes

---
+++ RubikCube/text.cpp
@@ -0,0 +1,18 @@
+#include "text.h"
+#include <string>
+
+
+void make_text_vector_help(std::vector<std::string>& text_vector) {
+    text_vector.push_back("W - UP");
+    text_vector.push_back("A - LEFT");
+    text_vector.push_back("S - FRONT");
+    text_vector.push_back("D - RIGHT");
+    text_vector.push_back("R - BACK");
+    text_vector.push_back("F - DOWN");
+    text_vector.push_back("1-5 - SPEED");
+    text_vector.push_back("O - DEMO MODE");
+    text_vector.push_back("P - RESET");
+    text_vector.push_back("M - MIX");
+    text_vector.push_back("N - RESET COUNTER");
+    text_vector.push_back("ARROWS - CHANGE POSITION");
+}
---
+++ RubikCube/text.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+void make_text_vector_help(std::vector<std::string>& text_vector);
